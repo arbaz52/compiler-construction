cf7907c801b752f247143017ae3d301a1e77de31
arbaz52 <arbaz5256@gmail.com>
2019-09-30 10:42:07 +0500

    lab3 is done, lab4 is in progress.

---
+++ Lab3/Lab3/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void find_constants(object sender, EventArgs e)
+        {
+            Regex rgx = new Regex(@"^([0-9]+(.[0-9]*))$");
+            //split
+            string[] words = inputField.Text.Split(' ');
+            for(int  i = 0; i < words.Length; i++)
+            {
+                if (rgx.Match(words[i]).Success)
+                {
+                    richTextBox1.Text += words[i] + " ";
+                }
+            }
+        }
+
+        private void find_keywords(object sender, EventArgs e)
+        {
+            Regex rgx = new Regex(@"^(int|float|char)$");
+            //split
+            string[] words = inputField.Text.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (rgx.Match(words[i]).Success)
+                {
+                    richTextBox1.Text += words[i] + " ";
+                }
+            }
+        }
+
+        private void activity3(object sender, EventArgs e)
+        {
+            Regex rgx = new Regex(@"^([tm])");
+            //split
+            string[] words = inputField.Text.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (rgx.Match(words[i]).Success)
+                {
+                    richTextBox1.Text += words[i] + " ";
+                }
+            }
+        }
+    }
+}
